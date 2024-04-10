7ac6c2b9dbaf8b621fec5f3178a8e9c3585141ec
Ewen Le Bihan <hey@ewen.works>
2024-04-10 13:03:01 +0200

    ✨ Implement basic create-narasimha package

--- packages/create/index.js
+++ packages/create/index.js
@@ -1,3 +1,5 @@
+import { writeFileSync, readFileSync } from 'fs';
+
 export const INTROSPECTION_QUERY = `
 query IntrospectionQuery {
 	__schema {
@@ -100,3 +102,55 @@ fragment TypeRef on __Type {
 }
 
 `;
+
+/**
+ *
+ * @param {string} version version constraint
+ * @param {string} cwd working directory of the package tha's being created
+ * @param {string} name name of the package to add
+ */
+export function addDevDependency(cwd, name, version) {
+	const pkg = JSON.parse(readFileSync(`${cwd}/package.json`, 'utf-8'));
+	const devDependencies = sortByKey({ ...pkg.devDependencies, [name]: `^${version}` });
+
+	writeFileSync(`${cwd}/package.json`, JSON.stringify({ ...pkg, devDependencies }, null, '\t'));
+}
+
+function sortByKey(obj) {
+	return Object.keys(obj)
+		.sort()
+		.reduce((acc, key) => {
+			acc[key] = obj[key];
+			return acc;
+		}, {});
+}
+
+/**
+ * Supports npm, pnpm, Yarn, cnpm, bun and any other package manager that sets the
+ * npm_config_user_agent env variable.
+ * Thanks to https://github.com/zkochan/packages/tree/main/which-pm-runs for this code!
+ */
+export function getPackageManager() {
+	if (!process.env.npm_config_user_agent) {
+		return undefined;
+	}
+	const user_agent = process.env.npm_config_user_agent;
+	const pm_spec = user_agent.split(' ')[0];
+	const separator_pos = pm_spec.lastIndexOf('/');
+	const name = pm_spec.substring(0, separator_pos);
+	return name === 'npminstall' ? 'cnpm' : name;
+}
+
+/**
+ *
+ * @param {require('recast').types.builders} b
+ * @param {string} name
+ * @param {string} source
+ * @returns
+ */
+export function importWithoutRename(b, name, source) {
+	return b.importDeclaration(
+		[b.importSpecifier(b.identifier(name), b.identifier(name))],
+		b.stringLiteral(source)
+	);
+}
